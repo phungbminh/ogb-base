19cbef56c8ee7dd0b747e628ea43c25bccd931e8
phungbminh <phungbminh@gmail.com>
2024-09-04 08:13:23 +0000

    update

--- graphproppred/run.sh
+++ graphproppred/run.sh
@@ -3,10 +3,10 @@ echo on
 
 
 # Step 2: Cài đặt các thư viện cần thiết
-#pip install -r /kaggle/working/ogb-base/graphproppred/setup.txt
+pip install -r /kaggle/working/ogb-base/graphproppred/setup.txt
 
 # Step 3: Chạy file main.py
-#python3 /kaggle/working/ogb-base/graphproppred/code2/main_pyg.py --gnn 'gin' --filename 'BestTrain' --epochs '10'
+python3 /kaggle/working/ogb-base/graphproppred/code2/main_pyg.py --gnn 'gin' --filename 'BestTrain' --epochs '10'
 
 # Step 4: Commit và push các thay đổi
 
