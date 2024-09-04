754b02cec3d81a7789e0ef02fb11614a8a5df9c6
phungbminh <phungbminh@gmail.com>
2024-09-04 07:47:06 +0000

    update

--- graphproppred/run.sh
+++ graphproppred/run.sh
@@ -15,7 +15,12 @@ echo on
 cd /
 cd /kaggle/working/ogb-base/
 
-git remote set-url origin https://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@github.com/phungbminh/ogb-base.git
+
+git config --global credential.helper manager
+git config --global credential.https://github.com.username phungbminh@gmail.com
+git config --global credential.https://github.com.password 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
+
+#git remote set-url origin https://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@github.com/phungbminh/ogb-base.git
 git add .
 git commit -m "update"
 git push origin main
